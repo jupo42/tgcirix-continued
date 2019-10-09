7773aa3e79a221532591b1dd338a40f1ae4e82c3
Justin Pope <jupo42@gmail.com>
2019-10-09 00:28:22 -0700

    sudo: updated to 1.8.27

--- sudo/build.sh
+++ sudo/build.sh
@@ -9,18 +9,20 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=sudo
-version=1.6.9p23
+version=1.8.27
 pkgver=1
-source[0]=ftp://ftp.sudo.ws/pub/sudo/OLD/$topdir-$version.tar.gz
+source[0]=ftp://ftp.sudo.ws/pub/sudo/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=sudo-1.8.27-mapanon.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
 # Global settings
-configure_args+=(--sysconfdir=$prefix/${_sysconfdir} --with-logging=syslog --with-logfac=auth --with-editor=/bin/vi --with-env-editor --with-ignore-dot --with-insults --with-all-insults --with-timedir=/var/run/sudo)
-ac_overrides="sudo_cv_func_isblank=no"
+configure_args+=(--sysconfdir=$prefix/${_sysconfdir} --with-logging=syslog --with-logfac=auth --with-editor=/bin/vi --with-env-editor --with-ignore-dot --with-insults --with-all-insults --with-rundir=/var/run/sudo --disable-rpath)
+
+# Makefile expects to be run as root, bypass for package creation
+export INSTALL_OWNER=""
 
 topinstalldir=/
 pkgdefprefix=${prefix:1}
@@ -40,7 +42,10 @@ build()
 reg install
 install()
 {
-    generic_install DESTDIR
+    clean stage
+    setdir ${srcdir}/${topsrcdir}
+    ${__make} install DESTDIR="$stagedir" INSTALL_OWNER=""
+
     doc BUGS HISTORY LICENSE README UPGRADE
     echo "$pkgdefprefix/${_sysconfdir}/sudoers config(suggest)" > $metadir/ops
     ${__rm} -f ${stagedir}${prefix}/libexec/*.la
@@ -62,6 +67,8 @@ install()
     done
     ${__rm} -f sudoedit.8
     ${__ln} -sf sudo.8 sudoedit.8
+    setdir ${stagedir}${prefix}/${_sysconfdir}
+    ${__rm} -f sudoers
 }
 
 reg pack
